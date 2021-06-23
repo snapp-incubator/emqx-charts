339ac7b170f7603c71c65af3608ebfab63a4c599
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-23 10:36:50 +0430

    Installed plugins in gatsby-config file

--- gatsby-blog/gatsby-config.js
+++ gatsby-blog/gatsby-config.js
@@ -1,17 +1,33 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `Gatsby learning site.`,
+    author: `@AmirhNajafz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-reading-time`,
+        ],
       },
     },
     `gatsby-transformer-sharp`,
