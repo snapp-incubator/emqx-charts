773db6dd0a6655404cf3eec0f6396ed8c958c935
amirhnajafiz <najafizadeh21@gmail.com>
2021-07-14 11:30:45 +0430

    Files refactor

--- gatsby-blog/src/pages/index.js
+++ gatsby-blog/src/pages/index.js
@@ -1,28 +1,10 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "components/layout"
-import Seo from "components/seo"
+// Components
+import Seo from "components/SEO"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    Start Page
   </Layout>
 )
 
